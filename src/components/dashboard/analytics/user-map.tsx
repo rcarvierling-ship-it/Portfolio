7cee04c73bb217556dde7600c78eca6332f14237
rcarvierling-ship-it <rcar.vierling@gmail.com>
2026-01-18 14:06:42 -0500

    Fix Geographies type error in UserMap

--- src/components/dashboard/analytics/user-map.tsx
+++ src/components/dashboard/analytics/user-map.tsx
@@ -64,7 +64,7 @@ export function UserMap({ data }: UserMapProps) {
             >
                 <ZoomableGroup zoom={1}>
                     <Geographies geography={geoUrl}>
-                        {({ geographies }) =>
+                        {({ geographies }: { geographies: any[] }) =>
                             geographies.map((geo) => (
                                 <Geography
                                     key={geo.rsmKey}
